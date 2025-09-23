a45a4d72df336aa9f2bafc6b67bfba12403c265a
Khushal-Rayrikar <khushal.rayrikar@gmail.com>
2025-09-23 21:14:18 +0530

    new/modified files

--- black hat python/tcp_client.py
+++ black hat python/tcp_client.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "10.38.240.139"
+target_port = 9998
 
 #create a socket object
 client = socket.socket(socket.AF_INET , socket.SOCK_STREAM) #1
@@ -10,7 +10,7 @@ client = socket.socket(socket.AF_INET , socket.SOCK_STREAM) #1
 client.connect((target_host,target_port))  #2
 
 #send some data
-client.send(b"GET / HTTP/1.1\nHOST: google.com\r\n\r\n") #3
+client.send(b"GET / TCP/1.1\nHOST: 10.38.240.139\r\n\r\n") #3
 
 #recive some date
 response = client.recv(4096)
